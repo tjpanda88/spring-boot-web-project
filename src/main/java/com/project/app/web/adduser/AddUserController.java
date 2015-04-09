0c10361d403bc5f11deb251b08213bceed39857f
tjpanda88 <tjpanda88@gmail.com>
2015-04-09 22:18:31 +0800

    Dozer

--- src/main/java/com/project/app/web/adduser/AddUserController.java
+++ src/main/java/com/project/app/web/adduser/AddUserController.java
@@ -2,10 +2,9 @@ package com.project.app.web.adduser;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import com.project.app.domain.model.User;
 import com.project.app.domain.service.user.UserService;
-import com.project.app.web.searchuser.SearchUserForm;
 
 @Slf4j
 @Controller
@@ -46,13 +44,14 @@ public class AddUserController {
 	@RequestMapping(value = "create", method = RequestMethod.GET)
     public String search(@Validated AddUserForm addUserForm,
             BindingResult result, Model model) {
-		
-		User user = new User();
-		user.setId(addUserForm.getId());
-		user.setName(addUserForm.getName());
-		user.setPassword(addUserForm.getPassword());
-		user.setType(addUserForm.getType());
-		
+		Mapper mapper = new DozerBeanMapper();
+		User user = (User) mapper.map(addUserForm, User.class);
+//		User user = new User();
+//		user.setId(addUserForm.getId());
+//		user.setName(addUserForm.getName());
+//		user.setPassword(addUserForm.getPassword());
+//		user.setType(addUserForm.getType());
+
 		userService.saveUser(user);
 		return "adduser/addForm";
 	}
